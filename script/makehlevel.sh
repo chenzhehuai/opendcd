b2ac122102a6ef281bf87b4c169a946897577b16
Zhehuai Chen <zchen@b01.clsp.jhu.edu>
2018-01-06 19:21:21 -0500

    unfini

--- script/makehlevel.sh
+++ script/makehlevel.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
-set -x
+set -xe
 LANG=$1
 EXP=$2
 GRAPH=$3
-KALDI_ROOT=../../../
+KALDI_ROOT=$4
 
 tscale=1.0
 loopscale=0.1
-N=3
+N=2
 P=1
+stage=2
 
 #required="$lang/L.fst $lang/G.fst $lang/phones.txt $lang/words.txt $lang/phones/silence.csl $lang/phones/disambig.int $model $tree"
 #for f in $required; do
 #  [ ! -f $f ] && echo "mkgraph.sh: expected $f to exist" && exit 1;
 #done
 
+export PATH=/export/a12/zchen/works/decoder/opendcd/3rdparty/openfst-src/src/bin/:$PATH
+export LD_LIBRARY_PATH=/export/a12/zchen/works/decoder/opendcd/3rdparty/openfst-src/src/lib/:/export/a12/zchen/works/decoder/opendcd//3rdparty/local/lib/fst/:$LD_LIBRARY_PATH
 
+
+
+if [ $stage -le 0 ]; then
+rm -rf ${GRAPH}
 mkdir -p ${GRAPH}
+fi
 
-if [ 1 == 2 ]; then
+if [ $stage -le 1 ]; then
+
 fstdeterminize ${LANG}/L_disambig.fst > ${GRAPH}/det.L.fst
 
 ${KALDI_ROOT}/src/fstbin/fstcomposecontext \
-  --context-size=3 --central-position=1 \
+  --context-size=$N --central-position=$P \
   --read-disambig-syms=${LANG}/phones/disambig.int \
-  --write-disambig-syms=${LANG}/disambig_ilabels_3_1.int \
-  ${GRAPH}/ilabels_3_1 ${GRAPH}/det.L.fst | fstarcsort > ${GRAPH}/CL.fst
+  --write-disambig-syms=${LANG}/disambig_ilabels_${N}_${P}.int \
+  ${GRAPH}/ilabels_${N}_${P} ${GRAPH}/det.L.fst | fstarcsort > ${GRAPH}/CL.fst
 
-./make-h-transducer --disambig-syms-out=${GRAPH}/h.disambig.int \
-  ${GRAPH}/ilabels_3_1 \
+make-h-transducer --disambig-syms-out=${GRAPH}/h.disambig.int \
+    --transition-scale=$tscale \
+  ${GRAPH}/ilabels_${N}_${P} \
   ${EXP}/tree \
   ${EXP}/final.mdl > ${GRAPH}/Ha.fst
+
+fi
+
+if [ $stage -le 2 ]; then
+
 
 fstdeterminize ${GRAPH}/Ha.fst > ${GRAPH}/det.Ha.fst
 
@@ -51,3 +66,4 @@ fstconvert --fst_type=olabel_lookahead \
 
 fstrelabel --relabel_opairs=${GRAPH}/g.irelabel ${LANG}/G.fst \
   | fstarcsort > ${GRAPH}/right.fst
+
