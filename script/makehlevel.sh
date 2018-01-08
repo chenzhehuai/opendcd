698355d8bee5b9616d3c9306f3abedcf8d34246f
Zhehuai Chen <zchen@b15.clsp.jhu.edu>
2018-01-08 11:30:11 -0500

    mod scripts

--- script/makehlevel.sh
+++ script/makehlevel.sh
@@ -6,10 +6,10 @@ GRAPH=$3
 KALDI_ROOT=$4
 
 tscale=1.0
-loopscale=0.1
+loopscale=1.0
 N=2
 P=1
-stage=2
+stage=3
 
 #required="$lang/L.fst $lang/G.fst $lang/phones.txt $lang/words.txt $lang/phones/silence.csl $lang/phones/disambig.int $model $tree"
 #for f in $required; do
@@ -40,30 +40,44 @@ make-h-transducer --disambig-syms-out=${GRAPH}/h.disambig.int \
     --transition-scale=$tscale \
   ${GRAPH}/ilabels_${N}_${P} \
   ${EXP}/tree \
-  ${EXP}/final.mdl > ${GRAPH}/Ha.fst
+  ${EXP}/final.mdl \
+  >${GRAPH}/Ha.fst
+#  | add-self-loops --self-loop-scale=1.0 --reorder=true ${EXP}/final.mdl - \
 
 fi
 
 if [ $stage -le 2 ]; then
 
 
-fstdeterminize ${GRAPH}/Ha.fst > ${GRAPH}/det.Ha.fst
+cat ${GRAPH}/Ha.fst > ${GRAPH}/det.Ha.fst
+#fstdeterminize ${GRAPH}/Ha.fst > ${GRAPH}/det.Ha.fst
 
-fstconvert --fst_type=olabel_lookahead \
-  --save_relabel_opairs=${GRAPH}/cl.irelabel ${GRAPH}/det.Ha.fst > ${GRAPH}/la.Ha.fst
-fstrelabel --relabel_ipairs=${GRAPH}/cl.irelabel ${GRAPH}/CL.fst | fstarcsort \
+#fstconvert --fst_type=olabel_lookahead \
+#  --save_relabel_opairs=${GRAPH}/cl.irelabel ${GRAPH}/det.Ha.fst > ${GRAPH}/la.Ha.fst
+cp ${GRAPH}/det.Ha.fst ${GRAPH}/la.Ha.fst
+#fstrelabel --relabel_ipairs=${GRAPH}/cl.irelabel ${GRAPH}/CL.fst \
+    cat ${GRAPH}/CL.fst \
+    | fstarcsort \
   | fstcompose ${GRAPH}/la.Ha.fst - > ${GRAPH}/det.HaCL.fst
-fi
 
 ${KALDI_ROOT}/src/fstbin/fstrmsymbols \
   ${GRAPH}/h.disambig.int ${GRAPH}/det.HaCL.fst \
   | ${KALDI_ROOT}/src/fstbin/fstrmepslocal | fstarcsort \
-  | ${KALDI_ROOT}/src/bin/add-self-loops --self-loop-scale=0.1 \
+  | ${KALDI_ROOT}/src/bin/add-self-loops --self-loop-scale=$loopscale \
   --reorder=true ${EXP}/final.mdl - > ${GRAPH}/HCL.fst
+fi
 
+if [ $stage -le 3 ]; then
 fstconvert --fst_type=olabel_lookahead \
   --save_relabel_opairs=${GRAPH}/g.irelabel ${GRAPH}/HCL.fst > ${GRAPH}/left.fst
 
-fstrelabel --relabel_opairs=${GRAPH}/g.irelabel ${LANG}/G.fst \
+fstrelabel --relabel_ipairs=${GRAPH}/g.irelabel ${LANG}/G.fst \
   | fstarcsort > ${GRAPH}/right.fst
 
+ fi
+if [ $stage -le 4 ]; then
+rm $GRAPH/words.txt 
+ ln -s ../../$LANG/words.txt $GRAPH/words.txt
+ #awk 'NR==FNR{d[$2]=$1}NR!=FNR{if (d[$2]==""){d[$2]=$2};print $1,d[$2]}' $GRAPH/g.irelabel $LANG/words.txt | sort -k 2n |awk '{print $1,NR-1}' > $GRAPH/words.txt
+ #awk 'NR==FNR{d[$1]=$2}NR!=FNR{if (d[$2]==""){d[$2]=$2};print $1,d[$2]}' $GRAPH/g.irelabel $LANG/words.txt | sort -k 2n |awk '{print $1,NR-1}' > $GRAPH/words.txt
+ fi
